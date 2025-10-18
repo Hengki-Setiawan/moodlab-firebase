8131b3f1247a1c577775822bb0ecd1fc6e457ff9
Hengki Setiawan <hengkisetiawan461@gmail.com>
2025-10-18 12:58:00 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5437363240-5a213",
+  "appId": "1:1080760609679:web:b1bbf2fbab06d30ff439fa",
+  "apiKey": "AIzaSyDKUg9lvcFcLCM6sKQb9NRzwlqBH8hiBPo",
+  "authDomain": "studio-5437363240-5a213.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1080760609679"
+};
